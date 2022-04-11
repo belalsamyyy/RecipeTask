78396ca2a6447ab3431a3259252a1398a04027b7
Belal Samy <38237387+belalsamyyy@users.noreply.github.com>
2022-04-11 21:10:16 +0200

    Init Softxpert task 🔥
    * Added .gitignore file

---
+++ SoftxpertTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoftxpertTask
+//
+//  Created by Belal Samy on 11/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
